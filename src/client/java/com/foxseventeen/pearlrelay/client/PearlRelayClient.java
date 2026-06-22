3385a11f773e75e0dfeddddcf86db90aaa79a6df
FoxSeventeen <59328125+FoxSeventeen@users.noreply.github.com>
2026-06-22 15:52:21 +0800

    Initial pearl relay template

---
+++ src/client/java/com/foxseventeen/pearlrelay/client/PearlRelayClient.java
@@ -0,0 +1,10 @@
+package com.foxseventeen.pearlrelay.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PearlRelayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
